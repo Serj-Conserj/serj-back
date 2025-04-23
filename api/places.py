792f8836cb012177e4be9294aa531c0b386f63e0
svbudygin <svbudygin@yandex.ru>
2025-04-23 14:00:07 +0300

    fix: added endpoint /places

---
+++ api/places.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from typing import List
+
+app = FastAPI()
+
+@app.get("/places")
+def get_places():
+    return [
+        {
+            "id": 1,
+            "name": "Пельменная №1",
+            "service_type": "Ресторан",
+            "metro": [{"id": 1, "name": "Пушкинская"}],
+            "cuisine": [{"id": 1, "name": "Русская"}]
+        },
+        {
+            "id": 2,
+            "name": "Sushi Go",
+            "service_type": "Кафе",
+            "metro": [{"id": 2, "name": "Таганская"}],
+            "cuisine": [{"id": 2, "name": "Японская"}]
+        }
+    ]
