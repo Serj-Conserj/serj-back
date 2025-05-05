e15a62159dd641499212d70d941967699465ab2d
svbudygin <svbudygin@gmail.com>
2025-05-05 11:43:47 +0300

    fix: webapp login

--- api/utils/schemas.py
+++ api/utils/schemas.py
@@ -3,6 +3,22 @@ from typing import List, Optional
 from uuid import UUID
 
 
+# ---------- Login ----------
+class TelegramAuth(BaseModel):
+    id: int
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    username: Optional[str] = None
+    photo_url: Optional[str] = None
+    auth_date: Optional[int] = None
+    hash: Optional[str] = None
+
+
+class RefreshRequest(BaseModel):
+    refresh: str
+
+
+# ---------- Places ----------
 class CuisineBaseSchema(BaseModel):
     name: str
 
